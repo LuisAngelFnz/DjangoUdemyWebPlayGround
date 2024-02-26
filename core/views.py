63ab229428a26e2106be23732e76987bf1838f05
Luis Angel <luisangel.fnz@gmail.com>
2024-02-25 23:34:04 -0600

    Primer lección de vistas basadas en funciones

--- core/views.py
+++ core/views.py
@@ -1,7 +1,26 @@
+from typing import Any
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-def home(request):
-    return render(request, "core/home.html")
-
-def sample(request):
-    return render(request, "core/sample.html")
+class HomePageView(TemplateView):    
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'core/home.html',
+            context={'title':'Home Page'},
+            *args,
+            **kwargs
+        )
+        
+    
+class SamplePageView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'core/sample.html',
+            context={'title':'Sample Page'},
+            *args,
+            **kwargs
+        )
