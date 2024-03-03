21bb176b364623c174b27894c6db911cdfd4c6ef
Luis Angel <luisangel.fnz@gmail.com>
2024-03-02 19:11:36 -0600

    Crear create pages y agrupamiento de url de pages

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,16 @@
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView
 from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
 from .models import Page
 
 # Create your views here.
 class PagesListView(ListView):
     model = Page
 
-class PageDetail(DetailView):
+class PageDetailView(DetailView):
     model = Page
+
+class PageCreateView(CreateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages:pages')
