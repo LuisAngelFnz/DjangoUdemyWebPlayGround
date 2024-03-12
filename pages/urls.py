e558d932c6735e0dd283e3276b9df88778d28b16
Luis Angel <luisangel.fnz@gmail.com>
2024-03-11 22:39:32 -0600

    Funcionalidad para editar pagina

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 pages_urlpatterns = [
     path('', views.PagesListView.as_view(), name='pages'),
-    path('<int:pk>/<slug:slug>/', views.PageDetailView.as_view(), name='page'),
+    path('<int:pk>/<slug:slug>', views.PageDetailView.as_view(), name='page'),
     path('create/', views.PageCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', views.PageUpdateView.as_view(), name='update')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView,CreateView, UpdateView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from .models import Page
@@ -14,3 +14,11 @@ class PageCreateView(CreateView):
     model = Page
     fields = ['title', 'content', 'order']
     success_url = reverse_lazy('pages:pages')
+
+class PageUpdateView(UpdateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    template_name_suffix = "_update_form"
+
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id])+'?ok'
