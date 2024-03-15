4a9f5d4476304e03976c7ff22fcf0dc5705d440d
Luis Angel <luisangel.fnz@gmail.com>
2024-03-14 23:56:55 -0600

    Agregar mix para operaciones CRUD

--- pages/views.py
+++ pages/views.py
@@ -1,21 +1,29 @@
-from django.views import generic as baseviews
+from django.views import generic as bv
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
 
+class StaffRequiredMix(object):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            print('Entrada no autorizada retornando a login')
+            return redirect(reverse_lazy('admin:login'))
+        return super().dispatch(request, *args, **kwargs)
+    
 # Create your views here.
-class PagesListView(baseviews.ListView):
+class PagesListView(StaffRequiredMix, bv.ListView):
     model = Page
 
-class PageDetailView(baseviews.detail.DetailView):
+class PageDetailView(StaffRequiredMix, bv.detail.DetailView):
     model = Page
 
-class PageCreateView(baseviews.CreateView):
+class PageCreateView(StaffRequiredMix, bv.CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
-
-class PageUpdateView(baseviews.UpdateView):
+    
+class PageUpdateView(StaffRequiredMix, bv.UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = "_update_form"
@@ -23,6 +31,6 @@ class PageUpdateView(baseviews.UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id])+'?ok'
 
-class PageDeleteView(baseviews.DeleteView):
+class PageDeleteView(bv.DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
