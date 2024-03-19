6bc9db1fa0dbb737ef6da0935772c081a2b6396f
Luis Angel <luisangel.fnz@gmail.com>
2024-03-18 23:47:52 -0600

    Cambiar decoradores sobre clases vista

--- pages/views.py
+++ pages/views.py
@@ -6,28 +6,23 @@ from django.utils.decorators import method_decorator
 from .models import Page
 from .forms import PageForm
 
-class StaffRequiredMix(object):
     
-    @method_decorator(staff_member_required)
-    def dispatch(self, request, *args, **kwargs):
-        # if not request.user.is_staff:
-        #     print('Entrada no autorizada retornando a login')
-        #     return redirect(reverse_lazy('admin:login'))
-        return super().dispatch(request, *args, **kwargs)
-    
-# Create your views here.
-class PagesListView(StaffRequiredMix, bv.ListView):
+@method_decorator(staff_member_required, name='dispatch')
+class PagesListView(bv.ListView):
     model = Page
 
-class PageDetailView(StaffRequiredMix, bv.detail.DetailView):
+@method_decorator(staff_member_required, name='dispatch')
+class PageDetailView(bv.detail.DetailView):
     model = Page
 
-class PageCreateView(StaffRequiredMix, bv.CreateView):
+@method_decorator(staff_member_required, name='dispatch')
+class PageCreateView(bv.CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
-    
-class PageUpdateView(StaffRequiredMix, bv.UpdateView):
+
+@method_decorator(staff_member_required, name='dispatch')   
+class PageUpdateView(bv.UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = "_update_form"
@@ -35,6 +30,7 @@ class PageUpdateView(StaffRequiredMix, bv.UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id])+'?ok'
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageDeleteView(bv.DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
