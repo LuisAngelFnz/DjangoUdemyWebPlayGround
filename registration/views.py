bf923789b427ae58744cf9da2f38a129a75389a3
Luis Angel <luisangel.fnz@gmail.com>
2024-05-27 09:18:18 -0600

    Modificar estética de la vista Login

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
@@ -9,3 +10,16 @@ class SignUpView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('login')+'?register'
+    
+    def get_form(self, *args):
+        formData = super(SignUpView, self).get_form(*args)
+        formData.fields['username'].widget = forms.TextInput(
+            attrs={'class':'form-control mb-2','placeholder':'Nombre de usuario'}
+        )
+        formData.fields['password1'].widget = forms.PasswordInput(
+            attrs={'class':'form-control mb-2','placeholder':'Contraseña'}
+        )
+        formData.fields['password2'].widget = forms.PasswordInput(
+            attrs={'class':'form-control mb-2','placeholder':'Repetir Contraseña'}
+        )
+        return formData
