9e17635e24db5809ace4069e844d2428c9c60687
Luis Angel Fernandez Vasquez <luisangel.fnz@gmail.com>
2024-06-03 14:50:57 -0600

    Crear y hacer requerido el campo de correo electrónico en en Login.

--- registration/views.py
+++ registration/views.py
@@ -1,10 +1,10 @@
 from django import forms
 from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreationFormWithEmail
 from django.urls import reverse_lazy
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
@@ -14,12 +14,15 @@ class SignUpView(CreateView):
     def get_form(self, *args):
         formData = super(SignUpView, self).get_form(*args)
         formData.fields['username'].widget = forms.TextInput(
-            attrs={'class':'form-control mb-2','placeholder':'Nombre de usuario'}
+            attrs={'class':'form-control mt-2','placeholder':'Nombre de usuario'}
+        )
+        formData.fields['email'].widget = forms.TextInput(
+            attrs={'class':'form-control mt-4','placeholder':'Correo electrónico'}
         )
         formData.fields['password1'].widget = forms.PasswordInput(
-            attrs={'class':'form-control mb-2','placeholder':'Contraseña'}
+            attrs={'class':'form-control mt-4','placeholder':'Contraseña'}
         )
         formData.fields['password2'].widget = forms.PasswordInput(
-            attrs={'class':'form-control mb-2','placeholder':'Repetir Contraseña'}
+            attrs={'class':'form-control mt-4','placeholder':'Repetir Contraseña'}
         )
         return formData
