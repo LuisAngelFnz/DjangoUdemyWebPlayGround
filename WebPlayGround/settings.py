ed5866aff9570831d17cf928f6098b66ca2a00f6
Luis Angel Fernandez Vasquez <luisangel.fnz@gmail.com>
2024-06-03 18:24:34 -0600

    Agregar funcionalidad de restablecer contraseña

--- WebPlayGround/settings.py
+++ WebPlayGround/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #Auth redirect
 LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
+
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'Mails/PassRecovery')
+else:
+    #Configurar en email para producción
+    pass
