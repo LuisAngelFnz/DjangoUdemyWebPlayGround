c8d0f85bfa23d0c4fa233d1d565354d8f3e36f22
Luis Angel Fernandez Vasquez <luisangel.fnz@gmail.com>
2024-06-10 11:57:18 -0600

    Crear perfil despudes de registrar un usuario mediante señales de django

--- registration/models.py
+++ registration/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
     user    = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar  = models.ImageField(upload_to='profiles', null=True, blank=True)
     bio     = models.TextField(null=True, blank=True)
     link    = models.URLField(max_length=200, null=True, blank=True)
+
+@receiver(post_save, sender=User)
+def ensure_profile_exists(sender, instance, **kwargs):
+    if kwargs.get('created'):
+        Profile.objects.get_or_create(user=instance)
+        print('Se acaba de crear un usuario y su perfil enlazado')
