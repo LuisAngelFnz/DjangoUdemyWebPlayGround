cc1938b744ec6a278ca89ec44433a9fa34e1c282
Luis Angel Fernandez Vasquez <luisangel.fnz@gmail.com>
2024-06-11 14:21:54 -0600

    Comenzar aplicación profiles

--- WebPlayGround/settings.py
+++ WebPlayGround/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'apps.registration',
     'apps.pages',
     'apps.core',
+    'apps.profiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
