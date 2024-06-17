5063a1688325e7d8ef653ae782ff6a34ab29ba43
Luis Angel <luisangel.fnz@gmail.com>
2024-06-17 08:59:52 -0600

    Avances en casa

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.shortcuts import get_object_or_404
 from registration.models import Profile
 
 class ListProfile(ListView):
     template_name = 'profiles/profile_list.html'
     model = Profile
+
+class DetailProfile(DetailView):
+    template_name = 'profiles/profile_detail.html'
+    model = Profile
+
+    def get_object(self) -> Profile:
+        return get_object_or_404(
+            Profile, user__username=self.kwargs['username']
+        )
