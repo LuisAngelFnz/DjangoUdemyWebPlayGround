cbf06c994a4107922513c3cd6870bd6605d77b7f
Luis Angel <luisangel.fnz@gmail.com>
2024-06-25 01:25:10 -0600

    Terminar DetailView de Profiles

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 profiles_urlpatterns = [
-    path('',  views.ListProfile.as_view(), name='profile'),
+    path('',  views.ListProfile.as_view(), name='profiles'),
     path('detail/<slug:username>/',  views.DetailProfile.as_view(), name='detail')
 ]
