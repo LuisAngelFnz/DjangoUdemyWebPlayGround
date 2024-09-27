f363ad5d7d123dc0805301da42be44f3aa626c24
Luis Angel <luisangel.fnz@gmail.com>
2024-09-26 23:41:26 -0600

    Bug solucionado cuando se importa en un test una app

--- apps/registration/tests.py
+++ apps/registration/tests.py
@@ -1,5 +1,5 @@
+from registration.models import Profile
 from django.test import TestCase
-from .models import Profile
 from django.contrib.auth.models import User
 
 class ProfileTestCase(TestCase):
