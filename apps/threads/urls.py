41678e5a6a5b5cbed7729531b4ea89d3654a666e
Luis Angel <luisangel.fnz@gmail.com>
2024-10-06 23:03:59 -0600

    Se crea el auto scroll de los mensajes y se agrega en tiempo real a la conversación el mensaje que se envia

--- apps/threads/urls.py
+++ apps/threads/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ThreadList, ThreadDetail
+from .views import ThreadList, ThreadDetail, addMessage
 
 threads_urlpatters = [
     path('', ThreadList.as_view(), name='list'),
-    path('thread/<int:pk>', ThreadDetail.as_view(), name='detail')
+    path('thread/<int:pk>', ThreadDetail.as_view(), name='detail'),
+    path('thread/<int:pk>/addMessage', addMessage, name='addMessage')
 ]
--- apps/threads/views.py
+++ apps/threads/views.py
@@ -1,19 +1,35 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView
 from django.views.generic import TemplateView
+from django.http import JsonResponse, Http404
+from django.shortcuts import get_object_or_404
 
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
-from .models import Thread
+from .models import Thread,Message
 
 @method_decorator(login_required, name='dispatch')
-class ThreadList(ListView):
-    # template_name = 'templates/thread_list.html'
-    model = Thread
-    def get_queryset(self):
-        querySet = super().get_queryset()
-        return querySet.filter(users=self.request.user)
+class ThreadList(TemplateView):
+    template_name = 'templates/thread_list.html'
 
 @method_decorator(login_required, name='dispatch')
 class ThreadDetail(DetailView):
     model = Thread
+
+
+def addMessage(request, pk):
+    response = {'created':False}
+    if not request.user.is_authenticated:
+        raise Http404('Usuario no autenticado')
+    
+    if request.method != 'GET':
+        raise Http404('Metodo no permitido')
+    
+    content = request.GET.get('content')
+    if content:
+        thread = get_object_or_404(Thread, pk=pk)
+        message = Message.objects.create(user=request.user, content=content)
+        thread.messages.add(message)
+        response['created'] = True
+
+    return JsonResponse(response)
